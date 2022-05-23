21090badbaf327aa036b448a59f4a920689f3c0d
Dylan Lim <dylan@Dylans-MacBook-Air-M1.local>
2022-05-23 13:20:19 +0800

    Edit docstrings for build_flow()

--- flow.py
+++ flow.py
@@ -4,6 +4,18 @@ from prefect.schedules import CronSchedule
 from functions import extract, write
 
 def build_flow():
+    """Creates and returns a Flow object. To run this file as a background process (pseudo-daemon),
+    1. SSH into remote server
+    2. cd into the right directory
+    3. Activate venv: `source venv/bin/activate`
+    4. Register the flow if not registered already: `python3 flow.py`
+    5. Run the Prefect agent using nohup: `sudo nohup prefect agent local start`
+    6. Exit terminal
+    
+    To kill the process:
+    1. Find the PID: `ps ax | grep prefect`
+    2. `kill pid`
+    """
     schedule = CronSchedule("0-59 * * * *")
 
     with Flow("test-flow", schedule=schedule) as flow:
